d01e2fc0f978255768ba6344b4e7935ba76d0380
이예담 <dam@iyedam-ui-MacBookAir.local>
2025-08-17 23:13:44 +0900

    style: tailwind 커스텀 컬러 등록 + 하단 네비 바 진짜 구현

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          100: "#A7D3CE",
+          200: "#006B5E",
+          300: "#1E4A45",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
